0c894cb331575ab25d74def8831217ffef01f8b9
Nicolas Lopez <nlopez97f@gmail.com>
2021-11-09 23:53:40 -0500

    primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const colors = require('colors');
+
+
+const crearArchivo = async( base = 5, listar = false, hasta = 10) => {
+
+    try{
+        let salida, consola = '';
+    
+        for(let i = 1; i<= hasta; i++){
+            salida += (`${ base } x ${ i } = ${ base * i }\n`);
+            consola += (`${ base } ${'x'.red} ${ i } ${'='.red} ${ colors.underline(base * i) }\n`);
+        }
+        
+        if(listar){
+            console.log('================================='.green);
+            console.log(`   Tabla del: ${ colors.blue(base) }`.green);
+            console.log('================================='.green);
+
+            console.log(consola);
+        }
+    
+        fs.writeFileSync( `./salida/tabla-${ base }.txt`, salida);
+    
+        return `tabla-${ base }.txt`;
+    } catch(err) {
+        throw err;
+    }
+
+}
+
+
+module.exports = {
+    crearArchivo
+}
+
+
